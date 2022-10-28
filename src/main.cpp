d3976953efe43ef1c2d11626ba884a579c8a7ae7
ayan4m1 <andrew@bulletlogic.com>
2022-10-28 11:58:04 -0400

    add RGB LED functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <Arduino.h>
 #include <Button2.h>
+#include <FastLED.h>
 
 #define PIN_LS 6
 #define PIN_SWITCH 7
+#define PIN_RGB_POWER 38
+#define PIN_RGB 39
 #define LS_FREQ 4e3
+#define NUM_RGB 1
 
+CRGB leds[NUM_RGB] = {CRGB::Orange};
 Button2 button = Button2();
 
-void pressed(Button2& btn) { tone(PIN_LS, LS_FREQ); }
+void pressed(Button2& btn) {
+  tone(PIN_LS, LS_FREQ);
+  leds[0] = CRGB::Green;
+}
 
-void released(Button2& btn) { noTone(PIN_LS); }
+void released(Button2& btn) {
+  noTone(PIN_LS);
+  leds[0] = CRGB::Red;
+}
 
 void note(uint32_t freq, uint16_t dur) {
   tone(PIN_LS, freq, dur);
@@ -25,11 +36,22 @@ void startupMelody() {
 }
 
 void setup() {
+  pinMode(PIN_RGB_POWER, OUTPUT);
+  digitalWrite(PIN_RGB_POWER, HIGH);
+
+  FastLED.addLeds<NEOPIXEL, PIN_RGB>(leds, NUM_RGB);
+  FastLED.show();
+
   button.setPressedHandler(pressed);
   button.setReleasedHandler(released);
   button.begin(PIN_SWITCH);
 
   startupMelody();
+
+  leds[0] = CRGB::Red;
 }
 
-void loop() { button.loop(); }
+void loop() {
+  button.loop();
+  FastLED.show();
+}
